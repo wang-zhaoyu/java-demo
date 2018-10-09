a9a19e421cd63290f2ac8bd0f2bfd2c282bcc2dd
wangzhaoyu <764667451@qq.com>
2018-10-09 11:33:51 +0800

    修改注释及删除不需要的代码

---
+++ demo-service/src/main/java/cn/joey/demo/service/common/DefaultProfileListener.java
@@ -0,0 +1,27 @@
+package cn.joey.demo.service.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author wangzhaoyu
+ * @date 2018/10/9 上午10:59
+ */
+public class DefaultProfileListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        String env = System.getProperty("spring.profiles.active");
+
+        //设置默认profile为test
+        if (StringUtils.isBlank(env)) {
+            System.setProperty("spring.profiles.active", "test");
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
--- demo-web/src/main/java/cn/joey/demo/web/controller/EchoController.java
+++ demo-web/src/main/java/cn/joey/demo/web/controller/EchoController.java
@@ -1,10 +1,9 @@
-package cn.mobilemart.demo.web.controller;
+package cn.joey.demo.web.controller;
 
 import cn.mobilemart.demo.api.EchoService;
 import cn.mobilemart.demo.web.common.ErrorCode;
 import cn.mobilemart.demo.web.common.MyPropertyPlaceholder;
 import cn.mobilemart.demo.web.common.Response;
-import com.baidu.disconf.client.usertools.DisconfDataGetter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,12 +38,6 @@ public class EchoController {
     public Response echo(@RequestParam(value = "param", required = false) String param) {
         Response<Map> result = new Response<>();
         Map<String, Object> data = new HashMap<String, Object>();
-        Object str=DisconfDataGetter.getByFileItem("dubbo.properties", "dubbo.zookeeper");
-        data.put("str",str);
-        String echo = echoService.echo(param);
-        data.put("param", param);
-        data.put("echo", echo);
-        data.put("datetime", new Date());
         result.setError_code(ErrorCode.SUCCESS.code);
         result.setErr_msg(ErrorCode.SUCCESS.message);
         result.setData(data);
